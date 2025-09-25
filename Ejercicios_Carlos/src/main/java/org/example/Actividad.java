15536986d77f3d5f1a6ffd6c5fbc97c96d662421
A711\dam1 <carloshuertascorrochano@gmail.com>
2025-09-25 19:15:19 +0200

    chais

---
+++ Ejercicios_Carlos/src/main/java/org/example/Actividad.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.util.Scanner;
+
+public class Actividad {
+
+    static void main(){
+        Scanner entrada = new Scanner(System.in);
+
+
+        System.out.println("como te llamas?: ");
+        String nombre = entrada.nextLine();
+
+        System.out.println("A que curso vas?: ");
+        String curso = entrada.nextLine();
+
+
+
+        System.out.println("hola " + nombre + " bienbenid@ al curso " + curso);
+
+
+
+        System.out.println("introduce un número...");
+        long num1 = entrada.nextLong();
+
+
+        System.out.println("introduce otro número...");
+        long num2 = entrada.nextLong();
+
+        long resultado = num1 + num2;
+        System.out.println("El resultado es " + resultado);
+
+    }
+
+
+}
