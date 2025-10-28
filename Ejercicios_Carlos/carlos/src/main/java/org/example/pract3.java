bbc0c492d85c8a4febd8d582c5f57cd4a40a0fb1
DESKTOP-0S8H00H\raulh <carloshuertascorrochano@gmail.com>
2025-10-28 15:24:41 +0100

    caca

---
+++ Ejercicios_Carlos/carlos/src/main/java/org/example/pract3.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class pract3 {
+
+    static void main() {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int num1 = 0;
+        int num2 = 0;
+        final int ALTURAMAX = 230;
+        final int ALTURAMIN = 140;
+        final int PESOMAX = 120;
+
+        System.out.println("Bienvenido al ratón juguetón.");
+
+
+
+        do {
+            try {
+                System.out.println("introduce la altura...");
+                num1 = entrada.nextInt();
+            }catch(Exception e) {
+                System.out.println("NO ES VALIDO");
+            }
+            if (num1 > ALTURAMAX || num1 < 0) {
+                System.out.println("ERROR DE LECTURA. BAJA DE LA BÁSCULA Y VUELVE A SUBIR");
+
+            }
+        } while (num1 < 0 || num1 > ALTURAMAX);
+
+
+        if (num1 < ALTURAMIN){
+
+            System.out.println("LO SIENTO, NO PUEDES MONTAR EN LA ATRACCIÓN");
+            int resultado1 = ALTURAMIN - num1;
+            System.out.println("TE FALTAN "+resultado1 +"cm");
+            System.exit(1);
+        }
+        try {
+            System.out.println("introduce el peso...");
+            num2 = entrada.nextInt();
+        }catch(Exception e) {
+            System.out.println("NO ES VALIDO");
+        }
+        final int PESOMIN = (num1 * 2) / 8;
+
+        if (num2 > PESOMIN && num2 < PESOMAX) {
+            System.out.println("¡¡SUBE A LA ATRACCIÓN!!");
+        }else{
+            if (num2 < PESOMIN){
+                int resultado = PESOMIN-num2;
+                System.out.println("TE FALTAN "+ resultado + " kg");
+                System.exit(1);
+
+            }else{
+                int resultado1 = num2-PESOMAX;
+                System.out.println("TE sobran "+ resultado1 + " kg");
+                System.exit(1);
+
+            }
+
+
+        }}}
